ce23c56d161a75ea79bf7ad5b24b345c5956f87f
Jerry Higgins <jerry@strideranalytical.com>
2018-06-14 19:14:07 -0700

    Created Component and prepared data

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Menu } from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          I just want to say <code>world/hello</code> reload.
-        </p>
+        <Navbar dark color="primary">
+          <div classNem="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />>
       </div>
     );
   }
