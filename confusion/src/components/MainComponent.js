fbda5bccb2d88881c89aae437bb67c447a326f4d
Jerry Higgins <jerry@strideranalytical.com>
2018-06-26 09:41:47 -0700

    Single Page Application Part 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -26,8 +27,16 @@ class Main extends Component {
     this.setState({ selectedDish: dishId});
   }
 
+
+
   render() {
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
 
     const HomePage = () => {
       return(
@@ -44,6 +53,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
         </Switch>
