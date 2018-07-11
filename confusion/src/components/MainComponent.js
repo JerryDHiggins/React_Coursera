eab74a89bd9fe1108f543f50b3406f892d7e32e2
Jerry Higgins <jerry@strideranalytical.com>
2018-07-11 07:57:06 -0700

    Added Aboutcomponent and navigation

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -53,6 +54,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
